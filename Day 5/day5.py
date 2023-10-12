b0081703d404165db982590ff0bb8aac41302b4d
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-12 15:22:51 +0400

    day 5

---
+++ Day 5/day5.py
@@ -0,0 +1,7 @@
+guys_sal=int(input("what is yo sal?"))
+if guys_sal > 10000:
+    print("did you study at goa?")
+elif guys_sal >10000 and guys_sal <10000:
+    print("yo mid")
+else:
+    print("why didnt yo joined goa")
